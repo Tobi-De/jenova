f1d5ca5057dc0baae9811eee7f921f5590c0bd6c
Tobi DEGNON <tobidegnon@protonmail.com>
2021-01-18 13:06:34 +0100

    add whitenoise

--- api.py
+++ api.py
@@ -6,20 +6,30 @@ from jinja2 import Environment, FileSystemLoader
 from parse import parse
 from requests import session as RequestsSession
 from webob import Request, Response
+from whitenoise import WhiteNoise
 from wsgiadapter import WSGIAdapter as RequestsWSGIAdapter
 
 
 class API:
-    def __init__(self, templates_dir="templates"):
+    def __init__(self, templates_dir="templates", static_dir="static"):
         self.routes = {}
 
         self.templates_env = Environment(
             loader=FileSystemLoader(os.path.abspath(templates_dir))
         )
 
+        self.exception_handler = None
+
+        self.whitenoise = WhiteNoise(self.wsgi_app, root=static_dir)
+
     def __call__(self, environ: Dict, start_response: Callable) -> Response:
-        request = Request(environ=environ)
+        return self.whitenoise(environ, start_response)
+
+    def wsgi_app(self, environ, start_response):
+        request = Request(environ)
+
         response = self.handle_request(request)
+
         return response(environ, start_response)
 
     def route(self, path: str):
@@ -40,16 +50,23 @@ class API:
     def handle_request(self, request: Request) -> Response:
         response = Response()
         handler, kwargs = self.find_handler(request_path=request.path)
-        if handler is not None:
-            if inspect.isclass(handler):
-                handler = getattr(handler(), request.method.lower(), None)
-                if handler is None:
-                    raise AttributeError(f"Method {request.method} not allowed")
-                handler(request, response, **kwargs)
+
+        try:
+            if handler is not None:
+                if inspect.isclass(handler):
+                    handler = getattr(handler(), request.method.lower(), None)
+                    if handler is None:
+                        raise AttributeError(f"Method {request.method} not allowed")
+                    handler(request, response, **kwargs)
+                else:
+                    handler(request, response, **kwargs)
             else:
-                handler(request, response, **kwargs)
-        else:
-            self.default_response(response)
+                self.default_response(response)
+        except Exception as e:
+            if self.exception_handler is None:
+                raise e
+            else:
+                self.exception_handler(request, response, e)
         return response
 
     def find_handler(self, request_path: str):
@@ -69,3 +86,5 @@ class API:
         session.mount(prefix=base_url, adapter=RequestsWSGIAdapter(self))
         return session
 
+    def add_exception_handler(self, exception_handler):
+        self.exception_handler = exception_handler
--- app.py
+++ app.py
@@ -45,3 +45,15 @@ def template_handler(req, resp):
     resp.body = app.template(
         "index.html", context={"name": "Jenova", "title": "Best Framework"}
     ).encode()
+
+
+def custom_exception_handler(request, response, exception_cls):
+    response.text = str(exception_cls)
+
+
+app.add_exception_handler(custom_exception_handler)
+
+
+@app.route("/exception")
+def exception_throwing_handler(request, response):
+    raise AssertionError("This handler should not be used.")
