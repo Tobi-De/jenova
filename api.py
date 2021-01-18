85c2572fde649c14fa1582d45132a2ae66dbb94b
Tobi DEGNON <tobidegnon@protonmail.com>
2021-01-18 09:45:36 +0100

    add jinja2 for templates and pytest for test

--- api.py
+++ api.py
@@ -1,9 +1,71 @@
-from typing import Dict, Callable, Iterable
+import inspect
+import os
+from typing import Dict, Callable
+
+from jinja2 import Environment, FileSystemLoader
+from parse import parse
+from requests import session as RequestsSession
+from webob import Request, Response
+from wsgiadapter import WSGIAdapter as RequestsWSGIAdapter
 
 
 class API:
-    def __call__(self, environ: Dict, start_response: Callable) -> Iterable:
-        response_body = b"Hello, world"
-        status = "200 0K"
-        start_response(status, headers=[])
-        return iter([response_body])
+    def __init__(self, templates_dir="templates"):
+        self.routes = {}
+
+        self.templates_env = Environment(
+            loader=FileSystemLoader(os.path.abspath(templates_dir))
+        )
+
+    def __call__(self, environ: Dict, start_response: Callable) -> Response:
+        request = Request(environ=environ)
+        response = self.handle_request(request)
+        return response(environ, start_response)
+
+    def route(self, path: str):
+        def wrapper(handler):
+            self.add_route(path, handler)
+            return handler
+
+        return wrapper
+
+    def add_route(self, path, handler):
+        assert path not in self.routes, "Such route already exists."
+        self.routes[path] = handler
+
+    def default_response(self, response: Response):
+        response.status_code = 404
+        response.text = "Not found."
+
+    def handle_request(self, request: Request) -> Response:
+        response = Response()
+        handler, kwargs = self.find_handler(request_path=request.path)
+        if handler is not None:
+            if inspect.isclass(handler):
+                handler = getattr(handler(), request.method.lower(), None)
+                if handler is None:
+                    raise AttributeError(f"Method {request.method} not allowed")
+                handler(request, response, **kwargs)
+            else:
+                handler(request, response, **kwargs)
+        else:
+            self.default_response(response)
+        return response
+
+    def find_handler(self, request_path: str):
+        for path, handler in self.routes.items():
+            parse_result = parse(path, request_path)
+            if parse_result is not None:
+                return handler, parse_result.named
+        return None, None
+
+    def template(self, template_name, context=None):
+        context = context or {}
+
+        return self.templates_env.get_template(template_name).render(context)
+
+    def test_session(self, base_url="http://testserver"):
+        session = RequestsSession()
+        session.mount(prefix=base_url, adapter=RequestsWSGIAdapter(self))
+        return session
+
--- app.py
+++ app.py
@@ -1,3 +1,47 @@
 from api import API
 
 app = API()
+
+
+@app.route("/home")
+def home(request, response):
+    response.text = "Hello from the HOME page"
+
+
+@app.route("/about")
+def about(request, response):
+    response.text = "Hello from the ABOUT page"
+
+
+@app.route("/hello/{name}")
+def greeting(request, response, name):
+    response.text = f"Hello, {name}"
+
+
+@app.route("/sum/{num_1:d}/{num_2:d}")
+def sum_(request, response, num_1, num_2):
+    total = int(num_1) + int(num_2)
+    response.text = f"{num_1} + {num_2} = {total}"
+
+
+@app.route("/book")
+class BooksResource:
+    def get(self, req, resp):
+        resp.text = "Books Page"
+
+    def post(self, req, resp):
+        resp.text = "Endpoint to create a book"
+
+
+def handler(req, resp):
+    resp.text = "sample"
+
+
+app.add_route("/sample", handler)
+
+
+@app.route("/template")
+def template_handler(req, resp):
+    resp.body = app.template(
+        "index.html", context={"name": "Jenova", "title": "Best Framework"}
+    ).encode()
